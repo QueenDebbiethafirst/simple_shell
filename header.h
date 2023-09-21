b59701736e28e3d9d1b4290f6029b5f25ed95b50
QueenDebbiethafirst <anuobisesan@gmail.com>
2023-09-20 21:55:00 -0700

    fail tst

--- header.h
+++ header.h
@@ -1,110 +1,53 @@
-nclude <unistd.h>
-#include <signal.h>
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <limits.h>
 #include <fcntl.h>
-#include <errno.h>
+#include <errno.h> 
 
-/* Definitions from the first header file */
-#define READ_BUF_SIZE 1024
+#define READ_BUFFER_SIZE 1024
 #define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
+#define CLEAR_BUFFER -1 
+
 #define CMD_NORM 0
 #define CMD_OR 1
 #define CMD_AND 2
-#define CMD_CHAIN 3
+#define CMD_CHAIN 3 
+
 #define CONVERT_LOWERCASE 1
-#define CONVERT_UNSIGNED 2
-#define USE_GETLINE 0
-#define USE_STRTOK 0
-#define HIST_FILE ".simple_shell_history"
-#define HIST_MAX 4096
-extern char **environ;
+#define CONVERT_UNSIGNED 2 
 
-typedef struct liststr {
-	    int num;
-	        char *str;
-		    struct liststr *next;
-} list_t;
-
-typedef struct passinfo {
-	    char *arg;
-	        char **argv;
-		    char *path;
-		        int argc;
-			    unsigned int line_count;
-			        int err_num;
-				    int linecount_flag;
-				        char *fname;
-					    list_t *env;
-					        list_t *history;
-						    list_t *alias;
-						        char **environ;
-							    int env_changed;
-							        int status;
-								    char **cmd_buf;
-								        int cmd_buf_type;
-									    int readfd;
-									        int histcount;
-} info_t;
-
-typedef struct builtin {
-	    char *type;
-	        int (*func)(info_t *);
-} builtin_table;
-
-/* Definitions from the second header file */
-#define CLEAR_BUFFER 0x1A
 #define USE_READ_LINE 0
+#define USE_STRTOK 0 
 
+
+/* Global Constants */
+#define WRITE_BUF_SIZE 1024 // You can adjust this value as needed
+#define HIST_FILE "event_log.c,fetch_history_file" // Replace with the actual history file name
+
+/* Data Structures */
 typedef struct info_t {
-	    char *arguments;
-	        char **arg_vector;
-		    char *executable_path;
-		        int arg_count;
-			    char *program_name;
-			        list_t *history;
-				    list_t *environment_data;
-				        list_t *alias_data;
-					    char **environment_strings;
-					        int input_stream;
-						    char **command_buffer;
-						        int hist_count;
-							    int env_data_changed;
-							        int linecount_flag;
+    // Define your info_t structure here based on the information provided
 } info_t;
 
-typedef struct list_s {
-	    char *nodestr;
-	        int histnum;
-		    struct list_s *next;
+typedef struct list_t {
+    int histnum;           // Node index utilized by history
+    char *nodestr;         // String field of the node
+    struct list_t *next;   // Pointer to the next node
 } list_t;
 
-/* Function declarations from the first header file */
-size_t get_list_length(const list_t *listHead);
-char **convert_list_to_strings(list_t *listPtr);
-size_t display_list_items(const list_t *listHead);
-list_t *element_starts_with(list_t *listPtr, char *prefixString, char charToCompare);
-ssize_t locate_element_index(list_t *listHead, list_t *node);
-int free_buffer(void **ptrToFree);
-int main(int argc, char **argv);
-char *initialize_memory(char *memptr, char fillbyte, unsigned int numbytes);
-void free_file(char **strarrptr);
-void *_realloc(void *memblockptr, unsigned int oldsize, unsigned int newsize);
-int check_command(info_t *info, char *filepath);
-char *duplicate_characters(char *pstr, int st, int sp);
-char *locate_path(info_t *info, char *pstr, char *cmd);
+/* Function Prototypes */
 int command_line(info_t *info);
 int check_delimiter(char chr, char *delimiter_string);
 int char_is_alpha(int chr);
 int convert_to_integer(char *input_string);
-
-/* Function declarations from the second header file */
-char *copy_string(char *copydest, const char *appsrc, int copynum);
-char *concatenate_string(char *dest, const char *appsrc, int copynum);
-char *locate_char(const char *searchstr, char findchars);
 int exit_program(info_t *info);
 int switch_dir(info_t *info);
 int show_instructions(info_t *info);
@@ -118,13 +61,53 @@ char *retrieve_env(info_t *info, const char *env_var_name);
 int custom_set_environment(info_t *info);
 int delete_env_variable(info_t *info);
 int fill_env_list(info_t *info);
-void output_error(char *output_str);
-int write_char(char wc);
-int write_fd(char wc, int fd);
-int print_fd(char *output_str, int fd);
-int str_to_int_with_error_handling(char *sc);
-void eliminate_comments(char *buf);
-char *parse_number(long int convnum, int convbase, int flags);
+char *fetch_history_file(info_t *info);
+int record_history(info_t *info);
+int open_history(info_t *info);
+int create_history_list(info_t *info, char *data, int line_count);
+int update_history_numbers(info_t *info);
+list_t *insert_node(list_t **hptr, const char *nodestr, int histnum);
+list_t *insert_node_end(list_t **hptr, const char *nodestr, int histnum);
+size_t display_list_items_str(const list_t *firstnode);
+int remove_element_at_position(list_t **headp, unsigned int delindex);
+void release_list(list_t **headp);
+size_t get_list_length(const list_t *listHead);
+char **convert_list_to_strings(list_t *listPtr);
+size_t display_list_items(const list_t *listHead);
+list_t *element_starts_with(list_t *listPtr, char *prefixString, char charToCompare);
+ssize_t locate_element_index(list_t *listHead, list_t *node);
+void initialize_memory(char *memptr, char fillbyte, unsigned int numbytes);
+void free_file(char **strarrptr);
+void *_realloc(void *memblockptr, unsigned int oldsize, unsigned int newsize);
+ssize_t buffer_input(info_t *info, char **buf, size_t *lengthaddr);
+ssize_t fetch_input(info_t *info);
+ssize_t read_buffer(info_t *info, char *buf, size_t *bufsize);
+int read_line(info_t *info, char **bufptr, size_t *size);
+void sigintHandler(int signum);
+char **get_environment(info_t *info);
+int remove_environment_variable(info_t *info, char *remenv);
+int set_environment_variable(info_t *info, char *envvar, char *envval);
+void initialize_info_data(info_t *info);
+void populate_info_data(info_t *info, char **args);
+void free_info_data(info_t *info, int freeall);
+char *copy_string(char *copydest, const char *appsrc, int copynum);
+char *concatenate_string(char *dest, const char *appsrc, int copynum);
+char *locate_char(const char *searchstr, char findchars);
+unsigned int calculate_string_length(const char *strlen);
+int compare_strings(const char *str1, const char *str2);
+const char *starts_with_substring(const char *searchstr, const char *substr);
+char *concatenate_strings(char *combobuf, const char *appsrc);
+char *copy_string(char *destinationString, const char *sourceString);
+char *duplicate_string(const char *inputString);
+void print_string(const char *inputString);
+int print_character(char characterToWrite);
+char **split_string(char *inputStr, const char *delimiters);
+char **split_string_single_delimiter(char *inputStr, char delimiter);
+int check_chain_delimiter(info_t *info, char *buf, size_t *ptr);
+void determine_chain_execution(info_t *info, char *buf, size_t *ptr, size_t startpos, size_t buflen);
+int substitute_aliases(info_t *info);
+int substitute_variables(info_t *info);
+int substitute_string(char **oldstr, const char *newstr);
 
-#endif
-
+#endif 
+ 
