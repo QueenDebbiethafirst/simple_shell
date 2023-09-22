b4c568d726a10d2f5c27c4d9492931139f8a13fd
QueenDebbiethafirst <134192714+QueenDebbiethafirst@users.noreply.github.com>
2023-09-22 13:39:17 +0100

    Update mem_mgmt.c

--- mem_mgmt.c
+++ mem_mgmt.c
@@ -1,17 +1,18 @@
 #include "header.h"
+
 /**
-* free_buffer - Releases memory and sets the pointer to NULL.
-* @ptrToFree: Address of the pointer to be freed.
-*
-* Return: Returns 1 if memory was freed, 0 otherwise.
-*/
+ * free_buffer - Releases memory and sets the pointer to NULL.
+ * @ptrToFree: Address of the pointer to be freed.
+ *
+ * Return: Returns 1 if memory was freed, 0 otherwise.
+ */
 int free_buffer(void **ptrToFree)
 {
-if (ptrToFree && *ptrToFree)
-{
-free(*ptrToFree);
-*ptrToFree = NULL;
-return (1);
+    if (ptrToFree && *ptrToFree)
+    {
+        free(*ptrToFree);
+        *ptrToFree = NULL;
+        return (1);
+    }
+    return (0);
 }
-return (0);
-
