23c8bbeb8c78b422b5ca031d443b964b9838d7ab
QueenDebbiethafirst <anuobisesan@gmail.com>
2023-09-22 09:20:26 -0700

    maths

--- builtin_functions.c
+++ builtin_functions.c
@@ -1,169 +1,152 @@
 #include "header.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() 
+int main(void) 
 {
-    char buffer[10];
-    int num = 0;
-    int *ptr = NULL;
-
-    /*Read input from user*/
-    printf("Enter a number: ");
-    fgets(buffer, sizeof(buffer), stdin);
-    num = atoi(buffer);
-
-    /*Allocate memory for the pointer*/
-    ptr = (int*)malloc(sizeof(int));
-    if (ptr == NULL) {
-        printf("Failed to allocate memory\n");
-        return (1);
-    }
-
-    /*Assign value to the pointer*/
-    *ptr = num;
-
-    /* Print the value*/
-    printf("Value: %d\n", *ptr);
-
-    /*Free the allocated memory*/
-    free(ptr);
-
-    return (0);
+char buffer[10];
+int num = 0;
+int *ptr = NULL;
+/*Read input from user*/
+printf("Enter a number: ");
+fgets(buffer, sizeof(buffer), stdin);
+num = atoi(buffer);
+/*Allocate memory for the pointer*/
+ptr = (int*)malloc(sizeof(int));
+if (ptr == NULL) {
+printf("Failed to allocate memory\n");
+return (1);
 }
-
+/*Assign value to the pointer*/
+*ptr = num;
+/* Print the value*/
+printf("Value: %d\n", *ptr);
+/*Free the allocated memory*/
+free(ptr);
+return (0);
+}
 #include "header.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
 typedef struct info_s 
 {
-    char **argv;
+char **argv;
 } info_t;
-
 void output_error(info_t *info, char *msg) 
 {
-    fprintf(stderr, "%s", msg);
+fprintf(stderr, "%s", msg);
 }
-
 int str_to_int_with_error_handling(char *str) 
 {
-    char *endptr;
-    int num = strtol(str, &endptr, 10);
-    if (*endptr != '\0') 
-    {
-        return (-1);
-    }
-    return (num);
+char *endptr;
+int num = strtol(str, &endptr, 10);
+if (*endptr != '\0') 
+{
+return (-1);
 }
-
+return (num);
+}
 void print_char(char c) 
 {
-    putchar(c);
+putchar(c);
 }
-
 char* retrieve_env(info_t *info, char *prefix) 
 {
-    int i = 0;
-    while (info->argv[i] != NULL) 
-    {
-        if (strncmp(info->argv[i], prefix, strlen(prefix)) == 0) 
-        {
-            return info->argv[i] + strlen(prefix);
-        }
-        i++;
-    }
-    return (NULL);
+int i = 0;
+while (info->argv[i] != NULL) 
+{
+if (strncmp(info->argv[i], prefix, strlen(prefix)) == 0) 
+{
+return info->argv[i] + strlen(prefix);
 }
-
+i++;
+}
+return (NULL);
+}
 void _puts(char *str) 
 {
-    printf("%s", str);
+printf("%s", str);
 }
-
 int _strcmp(char *s1, char *s2) 
 {
-    return (strcmp(s1, s2));
+return (strcmp(s1, s2));
 }
-
 int exit_program(info_t *info) 
 {
-    int exit_code, err_code, execution_status;
-    if (info->argv[1]) 
-    {
-        exit_code = str_to_int_with_error_handling(info->argv[1]);
-        if (exit_code == -1) 
-        {
-            execution_status = 2;
-            output_error(info, "Illegal number: ");
-            output_error(info, info->argv[1]);
-            print_char('\n');
-            return (1);
-        }
-        err_code = str_to_int_with_error_handling(info->argv[1]);
-        return (-2);
-    }
-    err_code = -1;
-    return (-2);
+int exit_code, err_code, execution_status;
+if (info->argv[1]) 
+{
+exit_code = str_to_int_with_error_handling(info->argv[1]);
+if (exit_code == -1) 
+{
+execution_status = 2;
+output_error(info, "Illegal number: ");
+output_error(info, info->argv[1]);
+print_char('\n');
+return (1);
 }
-
+err_code = str_to_int_with_error_handling(info->argv[1]);
+return (-2);
+}
+err_code = -1;
+return (-2);
+}
 int switch_dir(info_t *info) 
 {
-    char *current_dir, *new_dir, buffer[1024];
-    int chdir_ret;
-    current_dir = getcwd(buffer, 1024);
-    if (!current_dir) 
-    {
-        output_error(info, "TODO: >>getcwd failure emsg here<<\n");
-    }
-    if (!info->argv[1]) 
-    {
-        new_dir = retrieve_env(info, "HOME=");
-        if (!new_dir) 
-        {
-            chdir_ret = chdir((new_dir = retrieve_env(info, "PWD=")) ? new_dir : "/");
-        } else 
-        {
-            chdir_ret = chdir(new_dir);
-        }
-    } else if (_strcmp(info->argv[1], "-") == 0) 
-    {
-        if (!retrieve_env(info, "OLDPWD=")) 
-        {
-            _puts(current_dir);
-            print_char('\n');
-            return (1);
-        }
-        _puts(retrieve_env(info, "OLDPWD="));
-        print_char('\n');
-        chdir_ret = chdir((new_dir = retrieve_env(info, "OLDPWD=")) ? new_dir : "/");
-    } 
-    else 
-    {
-        chdir_ret = chdir(info->argv[1]);
-    }
-    if (chdir_ret == -1) 
-    {
-        output_error(info, "can't cd to ");
-        output_error(info, info->argv[1]);
-        print_char('\n');
-    } else 
-    {
-        setenv("OLDPWD", retrieve_env(info, "PWD="), 1);
-        setenv("PWD", getcwd(buffer, 1024), 1);
-    }
-    return (0);
+char *current_dir, *new_dir, buffer[1024];
+int chdir_ret;
+current_dir = getcwd(buffer, 1024);
+if (!current_dir) 
+{
+output_error(info, "TODO: >>getcwd failure emsg here<<\n");
 }
-
+if (!info->argv[1]) 
+{
+new_dir = retrieve_env(info, "HOME=");
+if (!new_dir) 
+{
+chdir_ret = chdir((new_dir = retrieve_env(info, "PWD=")) ? new_dir : "/");
+} else 
+{
+chdir_ret = chdir(new_dir);
+}
+} else if (_strcmp(info->argv[1], "-") == 0) 
+{
+if (!retrieve_env(info, "OLDPWD=")) 
+{
+_puts(current_dir);
+print_char('\n');
+return (1);
+}
+_puts(retrieve_env(info, "OLDPWD="));
+print_char('\n');
+chdir_ret = chdir((new_dir = retrieve_env(info, "OLDPWD=")) ? new_dir : "/");
+} 
+else 
+{
+chdir_ret = chdir(info->argv[1]);
+}
+if (chdir_ret == -1) 
+{
+output_error(info, "can't cd to ");
+output_error(info, info->argv[1]);
+print_char('\n');
+} 
+else 
+{
+setenv("OLDPWD", retrieve_env(info, "PWD="), 1);
+setenv("PWD", getcwd(buffer, 1024), 1);
+}
+return (0);
+}
 int show_instructions(info_t *info) 
 {
-    char **arg_array = info->argv;
-    _puts("help call works. Function not yet implemented \n");
-    if (0) 
-    {
-        _puts(*arg_array);
-    }
-    return (0);
+char **arg_array = info->argv;
+_puts("help call works. Function not yet implemented \n");
+if (0) 
+{
+_puts(*arg_array);
+}
+return (0);
 }
--- header.h
+++ header.h
@@ -1,6 +1,5 @@
 #ifndef HEADER_H
 #define HEADER_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,7 +10,6 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
-
 /* Global Constants */
 #define WRITE_BUF_SIZE 1024
 #define CLEAR_BUFFER 0
@@ -27,269 +25,253 @@
 #define USE_STRTOK 0
 #define HIST_FILE ".simple_shell_history"
 #define HIST_MAX 4096
-
 extern char **environ;
-
 /**
- * struct liststr - singly linked list
- * @num: the number field
- * @str: a string
- * @next: points to the next node
- */
+* struct liststr - singly linked list
+* @num: the number field
+* @str: a string
+* @next: points to the next node
+*/
 typedef struct liststr {
-    int num;
-    char *str;
-    struct liststr *next;
+int num;
+char *str;
+struct liststr *next;
 } list_t;
-
 /**
- * struct passinfo - contains pseudo-arguments to pass into a function,
- * allowing uniform prototype for function pointer struct
- * @arg: a string generated from getline containing arguments
- * @argv: an array of strings generated from arg
- * @info: Address of the struct containing information.
- * @delimiter_string: The delimiter string to compare against.
- * @chr: a character
- * @input_string: The input string for conversion.
- * @alias_str: The alias string to be assigned.
- * @node: The alias node to be printed.
- * @env_var_name: Name of the environment variable.
- * @output_str: a string
- * @wc: The character to be written.
- * @fd: a file descriptor.
- * @sc: a string
- * @errstr: String containing the specified error type.
- * @printnum: The number to be printed.
- * @convnum: The number to be converted.
- * @convbase: The base for conversion.
- * @flags: Flags related to the arguments.
- * @copydest: The destination string for copying.
- * @appsrc: The source string.
- * @copynum: The number of characters to copy.
- * @dest: The destination string.
- * @searchstr: The string to search within.
- * @findchars: The character to locate.
- * @info: Parameter structure.
- * @buf: Address of the buffer.
- * @lengthaddr: Address of the length variable.
- * @buf: The buffer for storing data.
- * @bufsize: The size of the buffer.
- * @bufptr: Address of the pointer to the buffer, either preallocated or NULL.
- * @size: Size of the preallocated 'bufptr' buffer if not NULL.
- * @signum: The signal number.
- * @remenv: The environment variable's name to be removed.
- * @envvar: The name of the environment variable.
- * @envval: The value of the environment variable.
- * @args: Argument vector.
- * @freeall: Set to true if all fields need to be freed.
- * @data: The data.
- * @line_count: The count of history lines, hist_count.
- * @hptr: Address of a pointer to the head node.
- * @nodestr: String field of the node.
- * @histnum: Node index utilized by history.
- * @firstnode: Pointer to the first node.
- * @headp: Address of a pointer to the first node.
- * @delindex: Index of the node to be deleted.
- * @listHead: Pointer to the first node of the list.
- * @listPtr: Pointer to the first node of the list.
- * @prefixString: String to compare as a prefix.
- * @charToCompare: The succeeding character after the prefix to compare.
- * @argc: Number of command-line arguments.
- * @ptrToFree: Address of the pointer to be freed.
- * @filepath: The path to the file.
- * @pstr: The PATH string.
- * @st: The starting index.
- * @sp: The stopping index.
- * @cmd: The cmd to find
- * @memptr: Pointer to the memory area.
- * @fillbyte: The byte value to fill @memptr with.
- * @numbytes: The number of bytes to fill.
- * @strarrptr: Pointer to a string array.
- * @memblockptr: Pointer to a previously allocated memory block.
- * @oldsize: Size in bytes of the original block.
- * @newsize: Size in bytes of the new block.
- * @av: The argument vector passed from the main() function.
- * @strlen: Input string for which the length is to be determined.
- * @str1: The first string.
- * @str2: The second string.
- * @searchstr: The string to be searched.
- * @substr: The substring to be located within the 'searchstr' string.
- * @combobuf: The target buffer where the strings will be combined.
- * @appsrc: The source buffer holding the string to be appended.
- * @destinationString: Pointer to the destination string.
- * @sourceString: Pointer to the source string.
- * @inputString: The input string to be duplicated.
- * @characterToWrite: The character to be written.
- * @inputStr: The input string to be split.
- * @delimiter: The delimiter character.
- * @ptr: A pointer to the current position within the buffer.
- * @inputStr: The input string to be split.
- * @delimiters: The delimiter string used for splitting.
- * @buf: A character buffer.
- * @ptr: A pointer to the current position within the buffer.
- * @startpos: The starting position within the buffer.
- * @buflen: The length of the buffer.
- * @oldstr: Address of old string.
- * @newstr: New string.
- * @buf: The address of the string to be modified.
- * @path: a string path for the current command
- * @argc: the argument count
- * @line_count: the error count
- * @err_num: the error code for exit()s
- * @linecount_flag: if on count this line of input
- * @fname: the program filename
- * @env: linked list local copy of environ
- * @environ: custom modified copy of environ from LL env
- * @history: the history node
- * @alias: the alias node
- * @env_changed: on if environ was changed
- * @status: the return status of the last exec'd command
- * @cmd_buf: address of pointer to cmd_buf, on if chaining
- * @cmd_buf_type: CMD_type ||, &&, ;
- * @readfd: the fd from which to read line input
- * @histcount: the history line number count
- */
+* struct passinfo - contains pseudo-arguments to pass into a function,
+* allowing uniform prototype for function pointer struct
+* @arg: a string generated from getline containing arguments
+* @argv: an array of strings generated from arg
+* @info: Address of the struct containing information.
+* @delimiter_string: The delimiter string to compare against.
+* @chr: a character
+* @input_string: The input string for conversion.
+* @alias_str: The alias string to be assigned.
+* @node: The alias node to be printed.
+* @env_var_name: Name of the environment variable.
+* @output_str: a string
+* @wc: The character to be written.
+* @fd: a file descriptor.
+* @sc: a string
+* @errstr: String containing the specified error type.
+* @printnum: The number to be printed.
+* @convnum: The number to be converted.
+* @convbase: The base for conversion.
+* @flags: Flags related to the arguments.
+* @copydest: The destination string for copying.
+* @appsrc: The source string.
+* @copynum: The number of characters to copy.
+* @dest: The destination string.
+* @searchstr: The string to search within.
+* @findchars: The character to locate.
+* @info: Parameter structure.
+* @buf: Address of the buffer.
+* @lengthaddr: Address of the length variable.
+* @buf: The buffer for storing data.
+* @bufsize: The size of the buffer.
+* @bufptr: Address of the pointer to the buffer, either preallocated or NULL.
+* @size: Size of the preallocated 'bufptr' buffer if not NULL.
+* @signum: The signal number.
+* @remenv: The environment variable's name to be removed.
+* @envvar: The name of the environment variable.
+* @envval: The value of the environment variable.
+* @args: Argument vector.
+* @freeall: Set to true if all fields need to be freed.
+* @data: The data.
+* @line_count: The count of history lines, hist_count.
+* @hptr: Address of a pointer to the head node.
+* @nodestr: String field of the node.
+* @histnum: Node index utilized by history.
+* @firstnode: Pointer to the first node.
+* @headp: Address of a pointer to the first node.
+* @delindex: Index of the node to be deleted.
+* @listHead: Pointer to the first node of the list.
+* @listPtr: Pointer to the first node of the list.
+* @prefixString: String to compare as a prefix.
+* @charToCompare: The succeeding character after the prefix to compare.
+* @argc: Number of command-line arguments.
+* @ptrToFree: Address of the pointer to be freed.
+* @filepath: The path to the file.
+* @pstr: The PATH string.
+* @st: The starting index.
+* @sp: The stopping index.
+* @cmd: The cmd to find
+* @memptr: Pointer to the memory area.
+* @fillbyte: The byte value to fill @memptr with.
+* @numbytes: The number of bytes to fill.
+* @strarrptr: Pointer to a string array.
+* @memblockptr: Pointer to a previously allocated memory block.
+* @oldsize: Size in bytes of the original block.
+* @newsize: Size in bytes of the new block.
+* @av: The argument vector passed from the main() function.
+* @strlen: Input string for which the length is to be determined.
+* @str1: The first string.
+* @str2: The second string.
+* @searchstr: The string to be searched.
+* @substr: The substring to be located within the 'searchstr' string.
+* @combobuf: The target buffer where the strings will be combined.
+* @appsrc: The source buffer holding the string to be appended.
+* @destinationString: Pointer to the destination string.
+* @sourceString: Pointer to the source string.
+* @inputString: The input string to be duplicated.
+* @characterToWrite: The character to be written.
+* @inputStr: The input string to be split.
+* @delimiter: The delimiter character.
+* @ptr: A pointer to the current position within the buffer.
+* @inputStr: The input string to be split.
+* @delimiters: The delimiter string used for splitting.
+* @buf: A character buffer.
+* @ptr: A pointer to the current position within the buffer.
+* @startpos: The starting position within the buffer.
+* @buflen: The length of the buffer.
+* @oldstr: Address of old string.
+* @newstr: New string.
+* @buf: The address of the string to be modified.
+* @path: a string path for the current command
+* @argc: the argument count
+* @line_count: the error count
+* @err_num: the error code for exit()s
+* @linecount_flag: if on count this line of input
+* @fname: the program filename
+* @env: linked list local copy of environ
+* @environ: custom modified copy of environ from LL env
+* @history: the history node
+* @alias: the alias node
+* @env_changed: on if environ was changed
+* @status: the return status of the last exec'd command
+* @cmd_buf: address of pointer to cmd_buf, on if chaining
+* @cmd_buf_type: CMD_type ||, &&, ;
+* @readfd: the fd from which to read line input
+* @histcount: the history line number count
+*/
 typedef struct info_t {
-    char *arg;
-    char **argv;
-    struct info_t *info;
-    char *delimiter_string;
-    char chr;
-    char *input_string;
-    char *alias_str;
-    struct liststr *node;
-    char *env_var_name;
-    char *output_str;
-    char wc;
-    int fd;
-    char *sc;
-    char *errstr;
-    int printnum;
-    int convnum;
-    int convbase;
-    int flags;
-    char *copydest;
-    const char *appsrc;
-    int copynum;
-    char *dest;
-    char *searchstr;
-    char findchars;
-    struct info_t *info;
-    char *buf;
-    int *lengthaddr;
-    char *buf;
-    size_t bufsize;
-    char **bufptr;
-    size_t size;
-    int signum;
-    char *remenv;
-    char *envvar;
-    char *envval;
-    char **args;
-    int freeall;
-    void *data;
-    int line_count;
-    struct liststr **hptr;
-    char *nodestr;
-    int histnum;
-    struct liststr *firstnode;
-    struct liststr **headp;
-    int delindex;
-    struct liststr *listHead;
-    struct liststr *listPtr;
-    char *prefixString;
-    char charToCompare;
-    int argc;
-    char **ptrToFree;
-    char *filepath;
-    char *pstr;
-    int st;
-    int sp;
-    char *cmd;
-    void *memptr;
-   ```c
-    char fillbyte;
-    size_t numbytes;
-    char **strarrptr;
-    void *memblockptr;
-    size_t oldsize;
-    size_t newsize;
-    char **av;
-    size_t strlen;
-    char *str1;
-    char *str2;
-    char *searchstr;
-    char *substr;
-    char *combobuf;
-    const char *appsrc;
-    char *destinationString;
-    const char *sourceString;
-    char *inputString;
-
+char *arg;
+char **argv;
+struct info_t *info;
+char *delimiter_string;
+char chr;
+char *input_string;
+char *alias_str;
+struct liststr *node;
+char *env_var_name;
+char *output_str;
+char wc;
+int fd;
+char *sc;
+char *errstr;
+int printnum;
+int convnum;
+int convbase;
+int flags;
+char *copydest;
+const char *appsrc;
+int copynum;
+char *dest;
+char *searchstr;
+char findchars;
+struct info_t *info;
+char *buf;
+int *lengthaddr;
+char *buf;
+size_t bufsize;
+char **bufptr;
+size_t size;
+int signum;
+char *remenv;
+char *envvar;
+char *envval;
+char **args;
+int freeall;
+void *data;
+int line_count;
+struct liststr **hptr;
+char *nodestr;
+int histnum;
+struct liststr *firstnode;
+struct liststr **headp;
+int delindex;
+struct liststr *listHead;
+struct liststr *listPtr;
+char *prefixString;
+char charToCompare;
+int argc;
+char **ptrToFree;
+char *filepath;
+char *pstr;
+int st;
+int sp;
+char *cmd;
+void *memptr;
+char fillbyte;
+size_t numbytes;
+char **strarrptr;
+void *memblockptr;
+size_t oldsize;
+size_t newsize;
+char **av;
+size_t strlen;
+char *str1;
+char *str2;
+char *searchstr;
+char *substr;
+char *combobuf;
+const char *appsrc;
+char *destinationString;
+const char *sourceString;
+char *inputString;
 typedef struct list_t list_t;
-
 typedef struct info_t {
-    char *arg;
-    char **argv;
-    char *delimiter_string;
-    char chr;
-    list_t *history;
-    list_t *alias;
-    int argc;
-    char *input_string;
+char *arg;
+char **argv;
+char *delimiter_string;
+char chr;
+list_t *history;
+list_t *alias;
+int argc;
+char *input_string;
 } info_t;
-
 typedef struct liststr {
-    int num;
-    char *str;
-    struct liststr *next;
+int num;
+char *str;
+struct liststr *next;
 } liststr_t;
-
 typedef struct passinfo {
-    char *arg;
-    char **argv;
-    char *path;
-    int argc;
-    unsigned int line_count;
-    int err_num;
-    int linecount_flag;
-    char *fname;
-    list_t *env;
-    list_t *history;
-    list_t *alias;
-    char **environ;
-    int env_changed;
-    int status;
-    char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-    int cmd_buf_type; /* CMD_type ||, &&, ; */
-    int readfd;
-    int histcount;
+char *arg;
+char **argv;
+char *path;
+int argc;
+unsigned int line_count;
+int err_num;
+int linecount_flag;
+char *fname;
+list_t *env;
+list_t *history;
+list_t *alias;
+char **environ;
+int env_changed;
+int status;
+char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+int cmd_buf_type; /* CMD_type ||, &&, ; */
+int readfd;
+int histcount;
 } passinfo_t;
-
 struct list_t {
-    int histnum;
-    char *str;
-    char *nodestr;
-    struct list_t *next;
+int histnum;
+char *str;
+char *nodestr;
+struct list_t *next;
 };
-
 #define INFO_INIT {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, 0, 0, 0}
-
 extern char **environ;
-
 char *copy_string(char *destinationString, const char *sourceString);
 char *copy_string(char *copydest, const char *appsrc, int copynum);
-
 #ifndef HEADER_H
 #define HEADER_H
-
 #include <stddef.h>
-
-typedef struct info_t {
-    char *type;
-    int (*func)(struct info_t *);
+typedef struct builtin_t {
+char *type;
+int (*func)(struct info_t *);
 } builtin_t;
-
 /* Function Prototypes */
 int command_line(struct info_t *info);
 int check_delimiter(char chr, char *delimiter_string);
@@ -359,5 +341,5 @@ void _putchar(char c);
 void _puts(const char *str);
 int substitute_variables(struct info_t *info);
 int substitute_string(char **oldstr, const char *newstr);
-
+#endif
 #endif
