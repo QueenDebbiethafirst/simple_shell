ed0d0c815faa59aea71a03de1be665fae69174d0
QueenDebbiethafirst <134192714+QueenDebbiethafirst@users.noreply.github.com>
2023-09-22 13:43:50 +0100

    Update reallocate_memory.c

--- reallocate_memory.c
+++ reallocate_memory.c
@@ -1,55 +1,68 @@
 #include "header.h"
+
 /**
-* initialize_memory - Fills a memory area with a specific byte value.
-* @memptr: Pointer to the memory area.
-* @fillbyte: The byte value to fill @memptr with.
-* @numbytes: The number of bytes to fill.
-*
-* Return: A pointer to the modified memory area (@memptr).
-*/
+ * initialize_memory - Fills a memory area with a specific byte value.
+ * @memptr: Pointer to the memory area.
+ * @fillbyte: The byte value to fill @memptr with.
+ * @numbytes: The number of bytes to fill.
+ *
+ * Return: A pointer to the modified memory area (@memptr).
+ */
 char *initialize_memory(char *memptr, char fillbyte, unsigned int numbytes)
 {
-unsigned int i;
-for (i = 0; i < numbytes; i++)
-memptr[i] = fillbyte;
-return (memptr);
+    unsigned int i;
+    for (i = 0; i < numbytes; i++)
+        memptr[i] = fillbyte;
+    return memptr;
 }
+
 /**
-* free_file - Frees memory occupied by an array of strings.
-* @strarrptr: Pointer to a string array.
-*/
+ * free_file - Frees memory occupied by an array of strings.
+ * @strarrptr: Pointer to a string array.
+ */
 void free_file(char **strarrptr)
 {
-char **arr = strarrptr;
-if (!strarrptr)
-return;
-while (*strarrptr)
-free(*strarrptr++);
-free(arr);
+    if (!strarrptr)
+        return;
+
+    char **arr = strarrptr;
+    while (*strarrptr)
+    {
+        free(*strarrptr);
+        strarrptr++;
+    }
+    free(arr);
 }
+
 /**
-* _realloc - Resizes a memory block.
-* @memblockptr: Pointer to a previously allocated memory block.
-* @oldsize: Size in bytes of the original block.
-* @newsize: Size in bytes of the new block.
-*
-* Return: Returns a pointer to the adjusted memory block.
-*/
+ * _realloc - Resizes a memory block.
+ * @memblockptr: Pointer to a previously allocated memory block.
+ * @oldsize: Size in bytes of the original block.
+ * @newsize: Size in bytes of the new block.
+ *
+ * Return: Returns a pointer to the adjusted memory block.
+ */
 void *_realloc(void *memblockptr, unsigned int oldsize, unsigned int newsize)
 {
-char *charptr;
-if (!memblockptr)
-return (malloc(newsize));
-if (!newsize)
-return (free(memblockptr), NULL);
-if (newsize == oldsize)
-return (memblockptr);
-charptr = malloc(newsize);
-if (!charptr)
-return (NULL);
-oldsize = oldsize < newsize ? oldsize : newsize;
-while (oldsize--)
-charptr[oldsize] = ((char *)memblockptr)[oldsize];
-free(memblockptr);
-return (charptr);
+    if (!memblockptr)
+        return malloc(newsize);
+
+    if (!newsize)
+    {
+        free(memblockptr);
+        return NULL;
+    }
+
+    if (newsize == oldsize)
+        return memblockptr;
+
+    char *charptr = malloc(newsize);
+    if (!charptr)
+        return NULL;
+
+    unsigned int copy_size = oldsize < newsize ? oldsize : newsize;
+    memcpy(charptr, memblockptr, copy_size);
+    free(memblockptr);
+
+    return charptr;
 }
