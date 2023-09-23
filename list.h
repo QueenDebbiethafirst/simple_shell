60ee1e7e52712a44bb83a988502e8f5906399d9e
QueenDebbiethafirst <134192714+QueenDebbiethafirst@users.noreply.github.com>
2023-09-23 05:55:53 +0100

    Update list.h

--- list.h
+++ list.h
@@ -5,6 +5,11 @@ typedef struct list_t {
     // Define the members of the list structure here
     char *str;
     struct list_t *next;
+char *nodestr;
+    int histnum;
+char *data;
+    int whether;
+    struct list_s *next;
 } list_t;
 
 #endif
