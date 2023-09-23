33b43d1feb544a00d2a7678671b480552c136573
QueenDebbiethafirst <134192714+QueenDebbiethafirst@users.noreply.github.com>
2023-09-22 21:23:54 -0700

    Update variables_mgmt.c

--- variables_mgmt.c
+++ variables_mgmt.c
@@ -1,11 +1,56 @@
 #include "header.h"
-
+#include <stdio.h>
 /**
 * user_environment - Displays the current environment.
 * @info: A structure holding potential arguments, ensuring a consistent function prototype.
 *
 * Return: Always returns 0.
 */
+
+#include <stdio.h>
+
+/* Define your list_t struct and other necessary data structures here. */
+
+/* Function prototypes */
+void display_list_items_str(list_t *list);
+int starts_with(const char *str, const char *prefix);
+void output_error(const char *error_message);
+void insert_node_end(list_t **list, const char *str, int value);
+
+/* Declare Info_env_data at the beginning of the file or in a header file. */
+list_t *Info_env_data = NULL; /* Line added to declare Info_env_data */
+
+void fill_env_list(void) {
+    /* Rest of your code to populate the list */
+    Info_env_data = node; /* Assign the list to Info_env_data */
+}
+
+void retrieve_env(const char *env_var_name) 
+{
+    list_t *node = Info_env_data;
+    int value;
+
+    while (node != NULL) {
+        if (starts_with(node->str, env_var_name)) 
+        {
+            value = starts_with(node->str, env_var_name); /* Line corrected, assign value correctly */
+            /* Process value or do something with it */
+        }
+        node = node->next;
+    }
+}
+
+void custom_set_environment(void) 
+{
+    /* Rest of your code */
+}
+
+int main() {
+    /* Main program logic */
+    return 0;
+}
+
+
 int user_environment(info_t *info)
 {
 display_list_items_str(Info_env_data);
